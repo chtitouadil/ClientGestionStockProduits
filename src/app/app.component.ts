9fe3f2727fcc7eafb9ac1b560b45969a3ced94ee
chtitouadil <chtitou.adil@gmail.com>
2019-10-28 20:25:42 +0100

    ajout de sidebar et navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientGestionStockProduits';
+  showHideSideBar: boolean;
+  onshowSideBarChange(showHideSideBar){
+    this.showHideSideBar= showHideSideBar;
+  }
+
 }
